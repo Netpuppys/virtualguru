c84140331a9493757e90d81f2bccb5660407c809
Harsh Kumar rattewal <harshrattewal@gmail.com>
2025-02-03 10:05:54 +0530

    ignored out.zip

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,51 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  // ssg: false,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "virtualguru.online",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+
+  webpack: (config, { isServer }) => {
+    config.module.rules.push(
+      {
+        test: /\.(pdf)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[ext]",
+            publicPath: `/_next/static/files`,
+            outputPath: `${isServer ? "../" : ""}static/files`,
+          },
+        },
+      },
+      {
+        test: /\.(mp4|webm)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              publicPath: "/_next/static/images",
+              outputPath: "static/images",
+              name: "[name].[ext]",
+              esModule: false,
+            },
+          },
+        ],
+      }
+    );
+
+    return config;
+  },
+};
 
 export default nextConfig;
