6959f135973b8bd174c54e18b0251c0a38de2405
shourya mishra <shourya.desk@gmail.com>
2025-02-04 17:27:05 +0530

    gtag added

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      {/* <!-- Google Tag Manager --> */}
+      <Script
+          id="gtag-1"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T4M7X33H');
+            `,
+          }}
+        />
+      {/* <!-- End Google Tag Manager --> */}
 
       {/* <!-- Meta Pixel Code --> */}
       {/* Meta Pixel */}
@@ -54,6 +69,15 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T4M7X33H"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden"}}
+            ></iframe></noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         {children}
       </body>
     </html>
