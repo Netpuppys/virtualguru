9f7170090f9992cf901e9e8a1b06d49da8fb3405
shourya mishra <shourya.desk@gmail.com>
2025-02-04 16:12:44 +0530

    bug fix

--- app/layout.js
+++ app/layout.js
@@ -46,7 +46,7 @@ export default function RootLayout({ children }) {
           <img 
             height="1" 
             width="1" 
-            style="display:none"
+            style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=3520646018188374&ev=PageView&noscript=1"
           /></noscript>
       {/* <!-- End Meta Pixel Code --> */}
